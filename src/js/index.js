8a2c36a2a28208c3a17374ef5c8a7582d5a17260
Kirill Rusinov <kirill.rusinov190820@mail.ru>
2023-08-30 13:43:57 +0300

    Закончил проект

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,3 @@
-
 import add from "./modules/add";
 
 window.addEventListener('DOMContentLoaded', ()=>{
--- src/js/modules/add.js
+++ src/js/modules/add.js
@@ -7,6 +7,13 @@ function add() {
 
 	let tasks = [];
 
+	if (localStorage.getItem('tasks')) {
+		tasks = JSON.parse(localStorage.getItem('tasks'));
+
+		tasks.forEach(task => renderTask(task));
+	}
+
+	checkEmptyList();
 	// Добавили задачу
 	form.addEventListener('submit', addTask);
 
@@ -32,31 +39,16 @@ function add() {
 		//Добавляем задачу в массив с задачами
 		tasks.push(newTask);
 
-		//Формируем CSS класс
-		const cssClass = newTask.done ? 'task-title task-title--done' : 'task-title';
+		//Сохраняем список задач в локальное хранилище браузера
+		saveToLocalStorage();
 
-		console.log(tasks);
+		renderTask(newTask);
 
-		const taskHTML = `
-		<li id = '${newTask.id}' class="list-group-item d-flex justify-content-between task-item">
-				<span class="${cssClass}"> ${newTask.text}</span>
-				<div class="task-item__buttons">
-					<button type="button" data-action="done" class="btn-action">
-						<img src="./img/icons/tick.svg" alt="Done" width="18" height="18">
-					</button>
-					<button type="button" data-action="delete" class="btn-action">
-						<img src="./img/icons/cross.svg" alt="Done" width="18" height="18">
-					</button>
-				</div>
-			</li>
-		`;
-
-		tasksList.insertAdjacentHTML("beforeend", taskHTML);
+		//Очищаем поле ввода и возвращаем фокус на него
 		taskInput.value = '';
 		taskInput.focus();
-		if (tasksList.children.length > 1) {
-			emptyList.classList.add('none');
-		}
+
+		checkEmptyList();
 	}
 
 	function deleteTask(e) {
@@ -76,7 +68,7 @@ function add() {
 		// // Удаляем задачу из массива с задачами
 		// tasks.splice(index, 1);
 
-		//Удаляем задачу из массива с задачами с помощью фильтрации
+		//Удаляем задачу из массива с задачами с помощью фильтрации(создаем новый массив )
 		tasks = tasks.filter(task => {
 			if (task.id == id) {
 				return false;
@@ -85,14 +77,12 @@ function add() {
 			}
 		});
 
+		//Сохраняем список задач в локальное хранилище браузера
+		saveToLocalStorage();
+
 		//Удаляем задачу из разметки
 		parentNode.remove();
-
-		if (tasksList.children.length === 1) {
-			emptyList.classList.remove('none');
-		}
-
-
+		checkEmptyList();
 	}
 
 	function doneTask(e) {
@@ -102,15 +92,64 @@ function add() {
 
 		const parentNode = e.target.closest('.list-group-item');
 
-		const id = parentNode.id;
+		const id = Number(parentNode.id);
+
+		const task = tasks.find((task) => {
+			if (task.id == id) {
+				return true;
+			}
+		});
+
+		task.done = !task.done;
+
+		//Сохраняем список задач в локальное хранилище браузера
+		saveToLocalStorage();
+
 		const taskTitle = parentNode.querySelector('.task-title');
 		taskTitle.classList.toggle('task-title--done');
-		console.log(id);
+
+	}
+
+	function checkEmptyList() {
+		if (tasks.length == 0) {
+			const emptyListHTML = `
+			<li id="emptyList" class="list-group-item empty-list">
+				<img src="./img/icons/leaf.svg" alt="Empty" width="48" class="mt-3">
+				<div class="empty-list__title">Список дел пуст</div>
+			</li>
+			`;
+			tasksList.insertAdjacentHTML('afterbegin', emptyListHTML);
+		} else {
+			const emptyListEl = document.querySelector('#emptyList');
+			emptyListEl ? emptyListEl.remove() : null;
+		}
 	}
 
 	//Сохраняем в локальное хранилище
-	function saveToLocal(e) {
+	function saveToLocalStorage() {
+		localStorage.setItem('tasks', JSON.stringify(tasks));
+	}
 
+	function renderTask(task) {
+		//Формируем CSS класс
+		const cssClass = task.done ? 'task-title task-title--done' : 'task-title';
+
+		//Формируем разметку для новой задачи
+		const taskHTML = `
+				<li id = '${task.id}' class="list-group-item d-flex justify-content-between task-item">
+						<span class="${cssClass}"> ${task.text}</span>
+						<div class="task-item__buttons">
+							<button type="button" data-action="done" class="btn-action">
+								<img src="./img/icons/tick.svg" alt="Done" width="18" height="18">
+							</button>
+							<button type="button" data-action="delete" class="btn-action">
+								<img src="./img/icons/cross.svg" alt="Done" width="18" height="18">
+							</button>
+						</div>
+					</li>
+				`;
+		//Добавляем задачу на страницу
+		tasksList.insertAdjacentHTML("beforeend", taskHTML);
 
 	}
 
